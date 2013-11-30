eccbd207d14dc66e67ac805907a7fc78ac91ec6c
liyong.zly <liyong.zly@alibaba-inc.com>
2013-11-30 12:46:36 +0800

    upload my emacs and vim configuration

---
+++ emacs/.emacs.d/init.el
@@ -0,0 +1,20 @@
+(add-to-list 'load-path
+         "/disk10/liyong.zly/.emacs.d/site-lisp/ecb-alexott/")
+(require 'ecb)
+;(load "/disk10/liyong.zly/.emacs.d/config/cedet.el")
+(setq stack-trace-on-error nil)
+(setq ecb-auto-activate t
+      ecb-tip-of-the-day nil)
+
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(ecb-options-version "2.40"))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
