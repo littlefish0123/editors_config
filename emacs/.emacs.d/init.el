b6e6f03f7f8e39a1c019788fbd9ca3b7c72f8fbe
liyong.zly <liyong.zly@alibaba-inc.com>
2013-12-11 10:29:34 +0800

    change my emacs configuration

--- emacs/.emacs.d/init.el
+++ emacs/.emacs.d/init.el
@@ -1,10 +1,9 @@
 (add-to-list 'load-path
          "/disk10/liyong.zly/.emacs.d/site-lisp/ecb-alexott/")
 (require 'ecb)
-;(load "/disk10/liyong.zly/.emacs.d/config/cedet.el")
 (setq stack-trace-on-error nil)
-(setq ecb-auto-activate t
-      ecb-tip-of-the-day nil)
+(setq ecb-auto-activate t)
+(setq ecb-tip-of-the-day nil)
 
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
@@ -18,3 +17,59 @@
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
  )
+(add-to-list 'load-path
+             "/disk10/liyong.zly/.emacs.d/site-lisp/plugins/")
+(require 'session)
+  (add-hook 'after-init-hook 'session-initialize)
+(load "desktop") 
+(desktop-load-default) 
+(desktop-read)
+
+(require 'ibuffer)
+(global-set-key (kbd "C-x C-b") 'ibuffer)
+
+(global-set-key "%" 'match-paren)
+          
+(defun match-paren (arg)
+"Go to the matching paren if on a paren; otherwise insert %."
+  (interactive "p")
+  (cond ((looking-at "\\s\(") (forward-list 1) (backward-char 1))
+        ((looking-at "\\s\)") (forward-char 1) (backward-list 1))
+        (t (self-insert-command (or arg 1)))))
+
+(setq cscope-do-not-update-database t)
+(load-file "/disk10/liyong.zly/.emacs.d/site-lisp/plugins/xcscope.el")
+(require 'xcscope)
+;(require 'gtags)
+;shortcut key for global
+(global-set-key (kbd "C-c g f") 'gtags-find-tag)  
+(global-set-key (kbd "C-c g p") 'gtags-pop-stack)  
+(global-set-key (kbd "C-c g s") 'gtags-select-tag)  
+
+(global-set-key (kbd "C-c <left>") 'windmove-left)
+(global-set-key (kbd "C-c <right>") 'windmove-right)
+(global-set-key (kbd "C-c <up>") 'windmove-up)
+(global-set-key (kbd "C-c <down>") 'windmove-down)
+;;;; .....ecb..
+(global-set-key (kbd "C-c <f1>") 'ecb-hide-ecb-windows)
+(global-set-key (kbd "C-c <f2>") 'ecb-show-ecb-windows)
+;
+;;;;; ...ecb.....
+(global-set-key (kbd "C-<c1>") 'ecb-maximize-window-directories)
+(global-set-key (kbd "C-<c2>") 'ecb-maximize-window-sources)
+(global-set-key (kbd "C-<c3>") 'ecb-maximize-window-methods)
+(global-set-key (kbd "C-<c4>") 'ecb-maximize-window-history)
+;;;;; ........
+(global-set-key (kbd "C-<c5>") 'ecb-restore-default-window-sizes)
+(global-set-key (kbd "C-<f3>")  'cscope-set-initial-directory)
+(global-set-key (kbd "C-<f4>")  'cscope-unset-initial-directory)
+(global-set-key (kbd "C-<f5>")  'cscope-find-this-symbol)
+(global-set-key (kbd "C-<f6>")  'cscope-find-global-definition)
+(global-set-key (kbd "C-<f7>")  'cscope-find-global-definition-no-prompting)
+(global-set-key (kbd "C-<f8>")  'cscope-pop-mark)
+(global-set-key (kbd "C-<f9>")  'cscope-next-symbol)
+(global-set-key (kbd "C-<f10") 'cscope-next-file)
+(global-set-key (kbd "C-<f11") 'cscope-prev-symbol)
+(global-set-key (kbd "C-<f12") 'cscope-prev-file)
+(global-set-key (kbd "meta f9")     'cscope-display-buffer)
+(global-set-key (kbd "meta f10")    'cscope-display-buffer-toggle)
